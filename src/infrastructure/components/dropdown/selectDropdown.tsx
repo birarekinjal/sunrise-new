f92f1bb6b36fdf0633365a8d6abe476214ec348a
Harish Chobisa <46278251+harishchobisa@users.noreply.github.com>
2022-03-03 12:48:25 +0530

    UI- Multiple select dropdown ui fix

--- src/infrastructure/components/dropdown/selectDropdown.tsx
+++ src/infrastructure/components/dropdown/selectDropdown.tsx
@@ -5,6 +5,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { SelectDropdownProps } from './dropdownModal';
+import './selectDropdown.scss';
 
 const SelectDropdown = ({
   options,
@@ -16,35 +17,33 @@ const SelectDropdown = ({
   showError,
 }: SelectDropdownProps) => {
   return (
-    <div>
-      <div>
-        <FormControl sx={{ m: 1, width: 300 }}>
-          <InputLabel id="demo-multiple-name-label">{label}</InputLabel>
-          <Select
-            labelId="demo-multiple-name-label"
-            id="demo-multiple-name"
-            multiple={multiple}
-            value={value}
-            onChange={onChange}
-            input={<OutlinedInput label="Name" />}
-          // MenuProps={MenuProps}
-          >
-            {options && options.length > 0 && options.map((data: any) => (
-              <MenuItem
-                key={data.name}
-                value={data.value}
-              >
-                {data.name}
-              </MenuItem>
-            ))}
-          </Select>
-          {showError && error && (
-            <>
-              <span>{error}</span>
-            </>
-          )}
-        </FormControl>
-      </div>
+    <div className='mb-2 select-dropdown'>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="demo-multiple-name-label">{label}</InputLabel>
+        <Select
+          labelId="demo-multiple-name-label"
+          id="demo-multiple-name"
+          multiple={multiple}
+          value={value}
+          onChange={onChange}
+          input={<OutlinedInput label="Name" />}
+        // MenuProps={MenuProps}
+        >
+          {options && options.length > 0 && options.map((data: any) => (
+            <MenuItem
+              key={data.name}
+              value={data.value}
+            >
+              {data.name}
+            </MenuItem>
+          ))}
+        </Select>
+        {showError && error && (
+          <>
+            <span>{error}</span>
+          </>
+        )}
+      </FormControl>
     </div>
   );
 };
