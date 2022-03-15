986efb200589dfeaccd71b5a29b9f9121702857d
birarekinjal <kinjal.birare@tntra.io>
2022-03-15 15:14:05 +0530

    Merge with main branch

--- src/presentation/modules/authentication/resetPassword/schema/resetPassword.tsx
+++ src/presentation/modules/authentication/resetPassword/schema/resetPassword.tsx
@@ -7,13 +7,13 @@ const resetPassword = Yup.object({
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
       'Must Contain 6 Characters, One Uppercase, One Lowercase, One Number and one special case Character',
     ),
-  confirm_password: Yup.string()
-    .required('This field is required.')
-    .oneOf([Yup.ref('password')], 'New password & confirm password must match.')
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-      'Must Contain 6 Characters, One Uppercase, One Lowercase, One Number and one special case Character',
-    ),
+  // confirm_password: Yup.string()
+  //   .required('This field is required.')
+  //   .oneOf([Yup.ref('password')], 'New password & confirm password must match.')
+  //   .matches(
+  //     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+  //     'Must Contain 6 Characters, One Uppercase, One Lowercase, One Number and one special case Character',
+  //   ),
 });
 
 export default resetPassword;
